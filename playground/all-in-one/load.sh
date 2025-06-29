a4d6e0d1dace5a3f019d0bc95111eddf47949efb
vule <ledangvu@gmail.com>
2025-06-29 21:57:59 +0700

    run load test in a container

--- playground/all-in-one/load.sh
+++ playground/all-in-one/load.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 
-#timeout 1000s bash -c 'for i in {1..10}; do (while true; do curl -s http://192.168.194.197:8080; done) & done; wait'
-bash -c 'for i in {1..10}; do (while true; do curl -s http://192.168.194.197:8080; done) & done; sleep 300 && pkill -P $$' &
-
-kubectl run -i --tty load-test --rm --image=busybox --restart=Never -- /bin/sh -c "while sleep 0.01; do wget -q -O- webapp-service:8080; done"
+kubectl run -i --tty hpa-test --rm --image=busybox --restart=Never -- /bin/sh -c "while sleep 0.01; do wget -q -O- webapp-service:8080; done"
